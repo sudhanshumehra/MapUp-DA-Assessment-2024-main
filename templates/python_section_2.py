8fb1d1be6327e43b4d632bcbc39e781c4efd6cd9
unknown <sudhanshu.mehra90@gmail.com>
2024-10-24 22:36:11 +0530

    Python section 2 assignment

--- templates/python_section_2.py
+++ templates/python_section_2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-
+import numpy as np
 
 def calculate_distance_matrix(df)->pd.DataFrame():
     """
@@ -12,9 +12,23 @@ def calculate_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Distance matrix
     """
     # Write your logic here
+    num_locations = df['id_1'].nunique()
+    distance_matrix = np.zeros((num_locations, num_locations))
 
+    for index, row in df.iterrows():
+        id_1, id_2, distance = row['id_1'], row['id_2'], row['distance']
+        distance_matrix[id_1 - 1, id_2 - 1] = distance
+        distance_matrix[id_2 - 1, id_1 - 1] = distance
+
+        for k in range(num_locations):
+            for i in range(num_locations):
+             for j in range(num_locations):
+                if distance_matrix[i, j] == 0 and i != j:
+                    distance_matrix[i, j] = distance_matrix[i, k] + distance_matrix[k, j]
+
+    df_distance_matrix = pd.DataFrame(distance_matrix, index=df['id_1'].unique(), columns=df['id_1'].unique())
     return df
-
+    
 
 def unroll_distance_matrix(df)->pd.DataFrame():
     """
@@ -27,9 +41,11 @@ def unroll_distance_matrix(df)->pd.DataFrame():
         pandas.DataFrame: Unrolled DataFrame containing columns 'id_start', 'id_end', and 'distance'.
     """
     # Write your logic here
-
+    ids = df.index.unique()
+    combinations = [(id_start, id_end) for id_start in ids for id_end in ids if id_start != id_end]
+    df_unrolled = pd.DataFrame(combinations, columns=['id_start', 'id_end'])
     return df
-
+    
 
 def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
     """
@@ -44,7 +60,12 @@ def find_ids_within_ten_percentage_threshold(df, reference_id)->pd.DataFrame():
                           of the reference ID's average distance.
     """
     # Write your logic here
-
+    average_distance = df['distance'].mean()
+    threshold_lower = average_distance * 0.9
+    threshold_upper = average_distance * 1.1
+    df_filtered = df[(df['id_start'] != reference_id) & (df['distance'] >= threshold_lower) & (df['distance'] <= threshold_upper)]
+    ids_within_threshold = df_filtered['id_start'].unique()
+    ids_within_threshold = sorted(ids_within_threshold)
     return df
 
 
@@ -59,6 +80,9 @@ def calculate_toll_rate(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Wrie your logic here
+    rate_coefficients = {'moto': 0.8, 'car': 1.2, 'rv': 1.5, 'bus': 2.2, 'truck': 3.6}
+    for vehicle_type, rate_coefficient in rate_coefficients.items():
+        df[vehicle_type] = df['distance'] * rate_coefficient
 
     return df
 
@@ -74,5 +98,4 @@ def calculate_time_based_toll_rates(df)->pd.DataFrame():
         pandas.DataFrame
     """
     # Write your logic here
-
     return df
